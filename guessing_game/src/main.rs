914a9b1dcb2ebb973cb86be88d96cd3e650fa015
justwei <admin@taylorswiftcn.com>
2022-02-17 22:47:52 +0800

    22-2-17

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,13 +1,35 @@
 
 use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
 
 fn main() {
-    println!("game start!");
-    println!("Guess a number from 1-100, please enter: ");
+    let random = rand::thread_rng().gen_range(1..101);
 
-    let mut guess = String::new();
-    io::stdin().read_line(&mut guess)
-    .expect("invaild value!");
+    loop {
+        println!("");
+        println!("game start!");
+        println!("Guess a number from 1-100, please enter: ");
 
-    println!("you input number: {}", guess);
+        let mut guess = String::new();
+        io::stdin().read_line(&mut guess)
+        .expect("failed to read line!");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("you input number: {}", guess);
+        // println!("random number: {}", &random);
+
+        match guess.cmp(&random) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            },
+        }
+    }
 }
