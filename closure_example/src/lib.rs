e8d95407dfc397bd4792c86742ee313139e464c0
justwei <admin@taylorswiftcn.com>
2022-04-08 00:15:15 +0800

    22-04-08

--- closure_example/src/lib.rs
+++ closure_example/src/lib.rs
@@ -1,13 +1,13 @@
-use std::collections::HashMap;
+use std::{collections::HashMap, hash::Hash};
 
 pub struct Cacher<T>
-where T: Fn(u32) -> u32 {
+    where T: Fn(u32) -> u32 {
     pub calculation: T,
     pub result: Option<u32>
 }
 
 impl<T> Cacher<T>
-where T: Fn(u32) -> u32, {
+    where T: Fn(u32) -> u32, {
     
     pub fn new(calculation: T) -> Cacher<T> {
         Cacher { calculation, result: None }
@@ -26,24 +26,51 @@ where T: Fn(u32) -> u32, {
 }
 
 pub struct BetterCacher<T>
-where T: Fn(u32) -> u32 {
+    where T: Fn(u32) -> u32 {
     pub calculation: T,
-    pub result: HashMap<u32, u32>
+    pub results: HashMap<u32, u32>
 }
 
 impl<T> BetterCacher<T>
-where T: Fn(u32) -> u32, {
+    where T: Fn(u32) -> u32, {
     
     pub fn new(calculation: T) -> BetterCacher<T> {
-        BetterCacher { calculation, result: HashMap::new() }
+        BetterCacher { calculation, results: HashMap::new() }
     }
 
     pub fn execute(&mut self, param: u32) -> u32 {
-        match self.result.get(&param) {
+        match self.results.get(&param) {
             Some(value) => *value,
             None => {
                 let value = (self.calculation)(param);
-                self.result.insert(param, value);
+                self.results.insert(param, value);
+                value
+            }
+        }
+    }
+}
+
+pub struct SuperCacher<T, K, V>
+    where T: Fn(K) -> V {
+    pub calculation: T,
+    pub results: HashMap<K, V>
+}
+
+impl<T, K, V> SuperCacher<T, K, V>
+    where T: Fn(K) -> V,
+    K: Hash + Eq + Copy,
+    V: Clone {
+    
+    pub fn new(calculation: T) -> SuperCacher<T, K, V> {
+        SuperCacher { calculation, results: HashMap::new() }
+    }
+
+    pub fn execute(&mut self, param: K) -> V {
+        match self.results.get(&param) {
+            Some(value) => value.clone(),
+            None => {
+                let value = (self.calculation)(param);
+                self.results.insert(param, value.clone());
                 value
             }
         }
--- closure_example/src/main.rs
+++ closure_example/src/main.rs
@@ -9,7 +9,7 @@ fn main() {
     let result_a = cacher.execute(10);
     let result_b = cacher.execute(20);
 
-    for ele in cacher.result {
+    for ele in cacher.results {
         println!("{}-{}", ele.0, ele.1)
     }
 
@@ -19,7 +19,7 @@ fn main() {
 
 #[cfg(test)]
 mod test {
-    use closure_example::{Cacher, BetterCacher};
+    use closure_example::{Cacher, BetterCacher, SuperCacher};
 
     
     #[test]
@@ -47,5 +47,25 @@ mod test {
 
         assert_eq!(11, result_a);
         assert_eq!(21, result_b);
+    }
+
+    #[test]
+    fn test_three() {
+        let mut int_cacher = SuperCacher::new(|args| {
+            args + 10
+        });
+
+        let result_a = int_cacher.execute(8);
+
+        let mut len_cacher = SuperCacher::new(|args: &str| {
+            let mut value = args.to_string();
+            value.push_str(".");
+            value
+        });
+
+        let result_b = len_cacher.execute("my name is justwei");
+
+        assert_eq!(18, result_a);
+        assert_eq!("my name is justwei.", result_b);
     }
 }
